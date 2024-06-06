1c534db847232eecb59b26d7fc306a22efa82c08
mohammad shobaki <mmshobaki23@gmail.com>
2024-06-06 15:12:46 -0500

    Added the geckodriver path explicitly and got functional tests working

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -1,6 +1,7 @@
 import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -11,7 +12,9 @@ MAX_WAIT = 5
 
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        geckodriver_path = "/snap/bin/firefox.geckodriver"
+        service = Service(geckodriver_path)
+        self.browser = webdriver.Firefox(service=service)
         test_server = os.environ.get("TEST_SERVER")
         if test_server:
             self.live_server_url = "http://" + test_server
